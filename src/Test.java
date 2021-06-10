41f3b1d697bf11a8c9346be902de8a9db8d7cf04
yzdg33 <839333009@qq.com>
2021-06-10 17:28:28 +0800

    First Commit all.

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName: Test
+ * @Description:
+ * @Author: Longi.liqitong
+ * @Date: 2021-6-10 17:25
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
